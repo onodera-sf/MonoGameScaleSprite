833223e1bff9a3bef218c06944420a0b60805dce
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-05-28 08:43:52 +0900

    プロジェクト ファイルを追加します。

---
+++ ScaleSprite/Game1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ScaleSprite
+{
+	/// <summary>
+	/// ゲームメインクラス
+	/// </summary>
+	public class Game1 : Game
+	{
+    /// <summary>
+    /// グラフィックデバイス管理クラス
+    /// </summary>
+    private readonly GraphicsDeviceManager _graphics = null;
+
+    /// <summary>
+    /// スプライトのバッチ化クラス
+    /// </summary>
+    private SpriteBatch _spriteBatch = null;
+
+    /// <summary>
+    /// テクスチャー
+    /// </summary>
+    private Texture2D _texture = null;
+
+
+    /// <summary>
+    /// GameMain コンストラクタ
+    /// </summary>
+    public Game1()
+    {
+      // グラフィックデバイス管理クラスの作成
+      _graphics = new GraphicsDeviceManager(this);
+
+      // ゲームコンテンツのルートディレクトリを設定
+      Content.RootDirectory = "Content";
+
+      // マウスカーソルを表示
+      IsMouseVisible = true;
+    }
+
+    /// <summary>
+    /// ゲームが始まる前の初期化処理を行うメソッド
+    /// グラフィック以外のデータの読み込み、コンポーネントの初期化を行う
+    /// </summary>
+    protected override void Initialize()
+    {
+      // TODO: ここに初期化ロジックを書いてください
+
+      // コンポーネントの初期化などを行います
+      base.Initialize();
+    }
+
+    /// <summary>
+    /// ゲームが始まるときに一回だけ呼ばれ
+    /// すべてのゲームコンテンツを読み込みます
+    /// </summary>
+    protected override void LoadContent()
+    {
+      // テクスチャーを描画するためのスプライトバッチクラスを作成します
+      _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+      // テクスチャーをコンテンツパイプラインから読み込む
+      _texture = Content.Load<Texture2D>("Texture");
+    }
+
+    /// <summary>
+    /// ゲームが終了するときに一回だけ呼ばれ
+    /// すべてのゲームコンテンツをアンロードします
+    /// </summary>
+    protected override void UnloadContent()
+    {
+      // TODO: ContentManager で管理されていないコンテンツを
+      //       ここでアンロードしてください
+    }
+
+    /// <summary>
+    /// 描画以外のデータ更新等の処理を行うメソッド
+    /// 主に入力処理、衝突判定などの物理計算、オーディオの再生など
+    /// </summary>
+    /// <param name="gameTime">このメソッドが呼ばれたときのゲーム時間</param>
+    protected override void Update(GameTime gameTime)
+    {
+      // ゲームパッドの Back ボタン、またはキーボードの Esc キーを押したときにゲームを終了させます
+      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+      {
+        Exit();
+      }
+
+      // TODO: ここに更新処理を記述してください
+
+      // 登録された GameComponent を更新する
+      base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// 描画処理を行うメソッド
+    /// </summary>
+    /// <param name="gameTime">このメソッドが呼ばれたときのゲーム時間</param>
+    protected override void Draw(GameTime gameTime)
+    {
+      // 画面を指定した色でクリアします
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+
+      // スプライトの描画準備
+      _spriteBatch.Begin();
+
+      // スプライトを普通に描画する
+      _spriteBatch.Draw(_texture, new Vector2(20.0f, 20.0f), null,
+          Color.White, 0.0f, Vector2.Zero, 1.0f,
+          SpriteEffects.None, 0.0f);
+
+      // スプライトを 0.5 倍にして描画する
+      _spriteBatch.Draw(_texture, new Vector2(20.0f, 170.0f), null,
+          Color.White, 0.0f, Vector2.Zero, 0.5f,
+          SpriteEffects.None, 0.0f);
+
+      // スプライトを２倍にして描画する
+      _spriteBatch.Draw(_texture, new Vector2(170.0f, 20.0f), null,
+          Color.White, 0.0f, Vector2.Zero, 2.0f,
+          SpriteEffects.None, 0.0f);
+
+      // スプライトを横に２倍、縦に 0.5 倍にして描画する
+      _spriteBatch.Draw(_texture, new Vector2(20.0f, 320.0f), null,
+          Color.White, 0.0f, Vector2.Zero, new Vector2(2.0f, 0.5f),
+          SpriteEffects.None, 0.0f);
+
+      // スプライトの一括描画
+      _spriteBatch.End();
+
+      // 登録された DrawableGameComponent を描画する
+      base.Draw(gameTime);
+    }
+  }
+}
